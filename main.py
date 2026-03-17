b0894b72397fdda5119a7f81e3dbf7af5dcd071d
Dawson Derian <dawsonderian@mac.home>
2026-03-17 22:55:45 +0100

    fix main.py syntax + migration WeasyPrint

--- main.py
+++ main.py
@@ -43,7 +43,4 @@ def gen_pdf():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)with open("prompt_v4.txt", "r") as f:
-    base_prompt = f.read()
-
-full_prompt = base_prompt + "\n\nDonnées client:\n" + client_data
+    app.run(host='0.0.0.0', port=port)
